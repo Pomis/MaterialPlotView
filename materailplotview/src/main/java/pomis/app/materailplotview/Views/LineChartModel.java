7361c37dd0ebdbbcaed91ad9b7215003d29c4229
Roman Ismagilov <pomis172@gmail.com>
2018-05-17 04:54:02 +0300

    LineChartView now does basic work

--- materailplotview/src/main/java/pomis/app/materailplotview/Views/LineChartModel.java
+++ materailplotview/src/main/java/pomis/app/materailplotview/Views/LineChartModel.java
@@ -1,17 +1,24 @@
 package pomis.app.materailplotview.Views;
 
 import java.lang.reflect.Field;
+import java.util.ArrayList;
 import java.util.List;
 
 class LineChartModel {
-    private List<Object> rawList;
+    private List rawList;
     private List<LineChartPoint> convertedList;
 
     private Field x;
     private Field y;
 
-    public void setList(List<Object> list) {
+    private int width;
+    private int height;
+
+    private int margin = 20;
+
+    public void setData(List list) {
         this.rawList = list;
+
 
         if (list != null && list.size() > 0) {
             Object o = list.get(0);
@@ -33,7 +40,39 @@ class LineChartModel {
                 y = field;
             }
         }
+
+        convertedList = new ArrayList<>();
+        for (Object obj : rawList) {
+            convertedList.add(new LineChartPoint(
+                    x.getInt(obj), y.getInt(obj)
+            ));
+        }
+
+
+
     }
+
+    public void recalc(int width, int height) {
+        this.width = width - margin;
+        this.height = height - margin;
+
+        int minX = convertedList.get(0).x;
+        int maxX = convertedList.get(0).x;
+        int minY = convertedList.get(0).y;
+        int maxY = convertedList.get(0).y;
+        for (LineChartPoint point : convertedList) {
+            if (point.x > maxX) maxX = point.x;
+            if (point.y > maxY) maxY = point.y;
+            if (point.x < minX) minX = point.x;
+            if (point.y < minY) minY = point.y;
+        }
+
+        for (LineChartPoint point : convertedList) {
+            point.x = margin/2 + (int)(((float)(point.x - minX)) / ((float)(maxX - minX)) * this.width);
+            point.y = margin/2 + this.height - (int)(((float)(point.y - minY)) / ((float)(maxY - minY)) * this.height);
+        }
+    }
+
 
     public List<LineChartPoint> getList() {
         return convertedList;
--- materailplotview/src/main/java/pomis/app/materailplotview/Views/LineChartView.java
+++ materailplotview/src/main/java/pomis/app/materailplotview/Views/LineChartView.java
@@ -2,8 +2,13 @@ package pomis.app.materailplotview.Views;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.LinearGradient;
 import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.Shader;
 import android.support.annotation.Nullable;
+import android.support.v4.graphics.ColorUtils;
 import android.util.AttributeSet;
 import android.view.View;
 
@@ -14,8 +19,7 @@ import pomis.app.materailplotview.R;
 public class LineChartView extends View {
     LineChartModel model = new LineChartModel();
     Paint accentPaint;
-    Paint primaryPaint;
-    Paint whitePaint;
+    Paint fillPaint;
     final int COLOR_ACCENT = getResources().getColor(R.color.colorAccent);
     final int COLOR_PRIMARY = getResources().getColor(R.color.colorPrimary);
 
@@ -23,34 +27,56 @@ public class LineChartView extends View {
         super(context, attrs);
     }
 
-    public LineChartView source(List<Object> list) {
+    public LineChartView source(List list) {
         //this.list = list;
-        this.model.setList(list);
+        this.model.setData(list);
         //initAdapter();
+        invalidate();
         return this;
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
+        model.recalc(getWidth(), getHeight());
         accentPaint = new Paint();
         accentPaint.setAntiAlias(true);
         accentPaint.setStyle(Paint.Style.STROKE);
         accentPaint.setColor(COLOR_ACCENT);
-        accentPaint.setStrokeWidth(15);
+        accentPaint.setStrokeWidth(5);
 
-        primaryPaint = new Paint();
-        primaryPaint.setAntiAlias(true);
-        primaryPaint.setStyle(Paint.Style.STROKE);
-        primaryPaint.setColor(COLOR_PRIMARY);
-        primaryPaint.setStrokeWidth(15);
-        if (model.getList().size() > 1)
+        fillPaint = new Paint();
+        fillPaint.setAntiAlias(true);
+        fillPaint.setStyle(Paint.Style.FILL);
+
+        fillPaint.setStrokeWidth(5);
+        fillPaint.setShader(new LinearGradient(0, getHeight() / 3 - getHeight(), 0, 9 * getHeight() / 10,
+                ColorUtils.compositeColors(COLOR_ACCENT, Color.TRANSPARENT), Color.TRANSPARENT, Shader.TileMode.CLAMP));
+
+
+        Path wallpath = new Path();
+        wallpath.reset();
+        wallpath.moveTo(model.getList().get(0).x, model.getList().get(0).y);
+
+        if (model != null && model.getList() != null && model.getList().size() > 1) {
             for (int i = 1; i < model.getList().size(); i++) {
                 LineChartPoint pointStart = model.getList().get(i - 1);
                 LineChartPoint pointEnd = model.getList().get(i);
+//                canvas.drawLine(pointStart.x, pointStart.y, pointEnd.x, pointEnd.y, fillPaint);
+                wallpath.lineTo(pointEnd.x, pointEnd.y);
                 canvas.drawLine(pointStart.x, pointStart.y, pointEnd.x, pointEnd.y, accentPaint);
             }
+            wallpath.lineTo(getWidth(), getHeight());
 
+            canvas.drawPath(wallpath, fillPaint);
+
+            accentPaint.setStyle(Paint.Style.FILL);
+
+            canvas.drawCircle(model.getList().get(model.getList().size() - 1).x,
+                    model.getList().get(model.getList().size() - 1).y,
+                    8, accentPaint
+            );
+        }
 
 //        postDelayed(animator, 15);
     }
